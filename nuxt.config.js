f21e63dedb994b8e87ebbff1fb8571863eafb11c
farjanaIslamNipa <nipa.farzana87@gmail.com>
2023-10-03 13:04:21 +0600

    Added title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: `${process.env.APP_NAME}`,
+    title: `${process.env.APP_NAME}` || "HiTasks",
     htmlAttrs: {
       lang: 'en'
     },
